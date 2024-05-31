a52232069959b3522f9fd6e5234ce4604c63f862
jenjun <jenjun97@gmail.com>
2024-05-31 21:00:15 +0800

    fix

--- reminder/reminder.js
+++ reminder/reminder.js
@@ -1,29 +1,13 @@
-//document.getElementById('reminderButton').addEventListener('click', function() {
-//    // 設置事件信息
-//    const title = '釗+均喜宴';
-//    const startTime = '20241019T040000Z'; // UTC時間，中午12點是UTC+8:00
-//    const endTime = '20241019T060000Z'; // 兩小時後
-//    const location = '324桃園市平鎮區延平路二段371號';
-//    const details = '聯絡人-謝政均 0909-367987';
-//
-//    // 生成Google Calendar URL (手機版)
-//    const url = `https://calendar.google.com/calendar/gp#~calendar:view=e&bm=1&text=${encodeURIComponent(title)}&dates=${startTime}/${endTime}&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}&trp=false`;
-//
-//    // 在新標籤頁打開URL
-//    window.open(url, '_blank');
-//});
-//
-//document.getElementById('iphoneReminderButton').addEventListener('click', function() {
-//    // 設置事件信息
-//    const title = '釗+均喜宴';
-//    const startTime = '20241019T120000'; // 當地時間，中午12點
-//    const endTime = '20241019T140000'; // 當地時間，兩小時後
-//    const location = '324桃園市平鎮區延平路二段371號';
-//    const details = '聯絡人-謝政均 0909-367987';
-//
-//    // 生成 iOS 行事曆 URL Scheme
-//    const url = `calshow://?title=${encodeURIComponent(title)}&start=${startTime}&end=${endTime}&location=${encodeURIComponent(location)}&notes=${encodeURIComponent(details)}`;
-//
-//    // 在新標籤頁打開URL
-//    window.open(url, '_blank');
-//});
+<div class="reminder-wrapper">
+    <!-- Google提醒按鈕 -->
+    <button type="button" class="btn btn-outline-success"
+        onclick="window.location.href='https://www.addevent.com/event/xY21860587+google'">
+        Google提醒
+    </button>
+
+    <!-- iPhone提醒按鈕 -->
+    <button type="button" class="btn btn-outline-info"
+        onclick="window.location.href='https://www.addevent.com/event/xY21860587+apple'">
+        iPhone提醒
+    </button>
+</div>
