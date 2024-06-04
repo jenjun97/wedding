482ddc412e6009843a704742090d007176096229
jenjun <2401078@iisigroup.com>
2024-06-04 17:54:43 +0800

    F

--- reminder/reminder.js
+++ reminder/reminder.js
@@ -1,15 +1,15 @@
 // reminder.js
 
-document.addEventListener('DOMContentLoaded', function() {
-    const reminderContent = document.getElementById('reminderContent');
-    if (reminderContent) {
-        fetch('reminder/reminder.html')
-            .then(response => response.text())
-            .then(data => {
-                reminderContent.innerHTML = data;
-            })
-            .catch(error => console.error('Error loading reminder.html:', error));
-    }
-});
+//document.addEventListener('DOMContentLoaded', function() {
+//    const reminderContent = document.getElementById('reminderContent');
+//    if (reminderContent) {
+//        fetch('reminder/reminder.html')
+//            .then(response => response.text())
+//            .then(data => {
+//                reminderContent.innerHTML = data;
+//            })
+//            .catch(error => console.error('Error loading reminder.html:', error));
+//    }
+//});
 
 // reminder.js其他代碼...
